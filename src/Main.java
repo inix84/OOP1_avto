c1a26e3f3e00bdd2cfd33c9118b8e5f5b6eac3be
inix84 <inix84@mail.ru>
2022-11-24 02:12:05 +0500

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         car BMWZ8 = new car("BMW", "Z8", 2021,"Германия", "черный",3.0);
         car KiaSportage4 = new car("Kia", "Sportage 4-го поколения", 2018,"Южная Корея", "красный",2.4);
         car HyundaiAvante = new car("Hyundai", "Avante", 2016,"Южная Корея", "оранжевый",1.6);
+        car NoName = new car(null, "", -2015,"", "",-1.7);
         System.out.println(LadaGranta);
         System.out.println(AudiA8);
         System.out.println(BMWZ8);
         System.out.println(KiaSportage4);
         System.out.println(HyundaiAvante);
-    }
+        System.out.println(NoName);
+       }
 }
--- src/car.java
+++ src/car.java
@@ -6,14 +6,38 @@ public class car {
     int productionYear;
     String productionCountry;
     car (String brand,String model, int productionYear, String productionCountry, String color, double engineVolume) {
+        if (brand!=null && brand.length()!=0) {
             this.brand = brand;
-            this.model=model;
-            this.engineVolume = engineVolume;
-            this.color = color;
-            this.productionYear = productionYear;
-            this.productionCountry = productionCountry;
+        } else {
+            this.brand = "default";
         }
-        @Override
+        if (model!=null && model.length()!=0) {
+            this.model=model;
+        } else {
+            this.model = "default";
+        }
+        if (engineVolume>0) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = 1.5;
+        }
+        if (color!=null && color.length()!=0) {
+            this.color = color;
+        } else {
+            this.color = "белый";
+        }
+        if (productionCountry!=null && productionCountry.length()!=0) {
+            this.productionCountry = productionCountry;
+        } else {
+            this.productionCountry = "default";
+        }
+        if (productionYear>0) {
+            this.productionYear = productionYear;
+        } else {
+            this.productionYear =2000 ;
+        }
+    }
+    @Override
         public String toString() {
             return "Автомобиль " + this.brand + " "+ this.model +", "+
                     this.productionYear+" года выпуска, сборка - "+this.productionCountry+
